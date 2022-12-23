5f5e5a74859cd5427cf9a6e3429dccff37270cc8
Laura Braker <thibmorozier@live.nl>
2022-12-23 16:15:41 +0100

    Add ArrayForEach nodes

---
+++ ChattyVibes/Nodes/ArrayNode/FloatArrayForEachNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.ArrayNode
+{
+    [STNode("/Array", "LauraRozier", "", "", "Float[] ForEach node")]
+    internal class FloatArrayForEachNode : ArrayForEachNode<float>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "Float[] ForEach";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_FLOAT);
+        }
+    }
+}
---
+++ ChattyVibes/Nodes/ArrayNode/UIntArrayForEachNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.ArrayNode
+{
+    [STNode("/Array", "LauraRozier", "", "", "UInt[] ForEach node")]
+    internal class UIntArrayForEachNode : ArrayForEachNode<uint>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "UInt[] ForEach";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_UINT);
+        }
+    }
+}
