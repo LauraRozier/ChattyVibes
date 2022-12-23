a4c3d1fee960cd8c1758d6454cc75cd51edd5b59
Laura Braker <thibmorozier@live.nl>
2022-12-23 17:22:07 +0100

    Add String Trim

---
+++ ChattyVibes/Nodes/StringNode/StringTrimEndNode.cs
@@ -0,0 +1,36 @@
+﻿using ST.Library.UI.NodeEditor;
+
+namespace ChattyVibes.Nodes.StringNode
+{
+    [STNode("/String", "LauraRozier", "", "", "String trim and node")]
+    internal sealed class StringTrimEndNode : StringNode
+    {
+        private string _value = "";
+
+        private STNodeOption m_op_in;
+        private STNodeOption m_op_out;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "String Trim End";
+
+            m_op_in = InputOptions.Add("", typeof(string), true);
+            m_op_out = OutputOptions.Add("", typeof(string), false);
+
+            m_op_in.DataTransfer += new STNodeOptionEventHandler(m_in_DataTransfer);
+            m_op_out.TransferData(_value);
+        }
+
+        private void m_in_DataTransfer(object sender, STNodeOptionEventArgs e)
+        {
+            if (e.Status == ConnectionStatus.Connected && e.TargetOption.Data != null)
+                _value = ((string)e.TargetOption.Data).TrimEnd();
+            else
+                _value = "";
+
+            SetOptionText(m_op_in, _value);
+            m_op_out.TransferData(_value);
+        }
+    }
+}
