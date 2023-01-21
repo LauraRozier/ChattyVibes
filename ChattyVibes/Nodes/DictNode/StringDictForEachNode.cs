5433d743ae9f21db0066981938802bc56854b181
Laura Braker <thibmorozier@live.nl>
2023-01-21 14:31:14 +0100

    Add string List and Dict

---
+++ ChattyVibes/Nodes/DictNode/StringDictForEachNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.DictNode
+{
+    //[STNode("/Dict", "LauraRozier", "", "", "Dict<String,String> ForEach node")]
+    internal class StringDictForEachNode : DictForEachNode<string, string>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "Dict<String,String> ForEach";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_STRING);
+        }
+    }
+}
---
+++ ChattyVibes/Nodes/ListNode/StringListForEachNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.ListNode
+{
+    //[STNode("/List", "LauraRozier", "", "", "List<String> ForEach node")]
+    internal class StringListForEachNode : ListForEachNode<string>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "List<String> ForEach";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_STRING);
+        }
+    }
+}
