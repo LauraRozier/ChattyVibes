1037258cb51c29f7f40bebee5673ba232d01d2a3
Laura Braker <thibmorozier@live.nl>
2023-01-21 14:16:54 +0100

    Add string array nodes

---
+++ ChattyVibes/Nodes/ArrayNode/StringArrayForEachNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.ArrayNode
+{
+    [STNode("/Array", "LauraRozier", "", "", "String[] ForEach node")]
+    internal class StringArrayForEachNode : ArrayForEachNode<string>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "String[] ForEach";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_STRING);
+        }
+    }
+}
---
+++ ChattyVibes/Nodes/ArrayNode/StringArrayNode.cs
@@ -0,0 +1,16 @@
+﻿using ST.Library.UI.NodeEditor;
+using System.Drawing;
+
+namespace ChattyVibes.Nodes.ArrayNode
+{
+    [STNode("/Array", "LauraRozier", "", "", "String[] node")]
+    internal class StringArrayNode : ArrayNode<string>
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Title = "String[]";
+            TitleColor = Color.FromArgb(200, Constants.C_COLOR_STRING);
+        }
+    }
+}
