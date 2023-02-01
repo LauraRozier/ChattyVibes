b6afc23283b82c57e3bad34dbcde364ee86957ba
Laura Braker <thibmorozier@live.nl>
2023-02-02 00:15:19 +0100

    Fix null-exception in aray ForEach

--- ChattyVibes/Nodes/ArrayNode/ArrayForEachNode.cs
+++ ChattyVibes/Nodes/ArrayNode/ArrayForEachNode.cs
@@ -10,9 +10,9 @@ namespace ChattyVibes.Nodes.ArrayNode
         protected T[] _data = null;
         protected STNodeOption m_op_flow_in;
         protected STNodeOption m_op_array_in;
-        protected STNodeOption m_op_flow_out_item;
+        protected STNodeOption m_op_flow_item_out;
         protected STNodeOption m_op_val_out_item;
-        protected STNodeOption m_op_flow_out_done;
+        protected STNodeOption m_op_flow_done_out;
 
         protected override void OnCreate()
         {
@@ -20,12 +20,12 @@ namespace ChattyVibes.Nodes.ArrayNode
 
             m_op_flow_in = InputOptions.Add(">", C_OBJ_TYPE, false);
             m_op_array_in = InputOptions.Add("IN", typeof(T[]), false);
-            m_op_flow_out_done = OutputOptions.Add("Done >", C_OBJ_TYPE, false);
-            m_op_flow_out_item = OutputOptions.Add("Item >", C_OBJ_TYPE, false);
+            m_op_flow_done_out = OutputOptions.Add("Done >", C_OBJ_TYPE, false);
+            m_op_flow_item_out = OutputOptions.Add("Item >", C_OBJ_TYPE, false);
             m_op_val_out_item = OutputOptions.Add("Item Value", typeof(T), false);
 
             m_op_flow_in.DataTransfer += new STNodeOptionEventHandler(m_op_DataTransfer);
-            m_op_flow_in.DataTransfer += new STNodeOptionEventHandler(m_op_DataTransfer);
+            m_op_array_in.DataTransfer += new STNodeOptionEventHandler(m_op_DataTransfer);
         }
 
         private void m_op_DataTransfer(object sender, STNodeOptionEventArgs e)
@@ -43,15 +43,18 @@ namespace ChattyVibes.Nodes.ArrayNode
 
         private void OnFlowTrigger()
         {
-            foreach (var item in _data)
+            if (_data != null)
             {
-                m_op_val_out_item.TransferData(item);
-                m_op_flow_out_item.TransferData(new object());
-                m_op_flow_out_item.TransferData(null);
+                foreach (var item in _data)
+                {
+                    m_op_val_out_item.TransferData(item);
+                    m_op_flow_item_out.TransferData(new object());
+                    m_op_flow_item_out.TransferData(null);
+                }
             }
 
-            m_op_flow_out_done.TransferData(new object());
-            m_op_flow_out_done.TransferData(null); // We reset with null to avoid triggering on changes
+            m_op_flow_done_out.TransferData(new object());
+            m_op_flow_done_out.TransferData(null); // We reset with null to avoid triggering on changes
         }
     }
 }
